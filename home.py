6ff9a88b0b64f3f981b5937acf47a38edb394360
Vinicius Eduardo <viniciuseduardo289@gmail.com>
2022-08-20 03:07:40 -0300

    v 1.1

--- home.py
+++ home.py
@@ -1,3 +1,6 @@
+pedido = []
+
+
 #Funções do programa
 
 def aplicacao():
@@ -6,7 +9,7 @@ def aplicacao():
 		lanches()
 	if escolha1 == 'nao':
 		print('Ok, aguardo sua escolha')
-		app()
+		aplicacao()
 
 def lanches():
 	print('''Menu de Lanches:
@@ -17,10 +20,16 @@ def lanches():
 	choose1 = input('Escolha seu lanche das opções acima: ')
 	if choose1 == '1':
 		print('Você escolheu opção: 1 - X-Tudo')
+		print('Agora, escolha sua bebida: ')
+		bebidas()
 	if choose1 == '2':
 		print('Você escolheu opção: 1 - X-Bacon')
+		print('Agora, escolha sua bebida: ')
+		bebidas()
 	if choose1 == '3':
 		print('Você escolheu opção: 1 - X-Egg')
+		print('Agora, escolha sua bebida: ')
+		bebidas()
 
 def bebidas():
 	print('''
@@ -28,6 +37,13 @@ def bebidas():
 		2 - Guaraná
 		3 - Fanta Uva
 	''')
+	choose2 = input('Digite aqui: ')
+	if choose2 == '1':
+		print('Você escolheu a bebida Coca-cola.')
+	if choose2 == '2':
+		print('Você escolheu a bebida Guaraná.')
+	if choose2 == '3':
+		print('Você escolheu a bebida Fanta Uva.')
 
 
 #Inicio do programa
